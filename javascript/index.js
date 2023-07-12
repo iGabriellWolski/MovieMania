c41b4fa0b0da695879c13c123652d39cfc1a51f4
iGabriellWolski <121004614+iGabriellWolski@users.noreply.github.com>
2023-07-12 12:44:32 -0300

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+const MenuIcon = document.querySelector(".js-fa-bars");
+
+
+MenuIcon.addEventListener("click",ToggleMenu);
+
+function ToggleMenu(){
+    MenuIcon.getBoundingClientRect(top)
+}
+
+ToggleMenu()
